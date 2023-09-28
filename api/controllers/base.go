bdf0632103ad0a5da10a08e37a50a2845d76e612
Azriel <azrielwah@gmail.com>
2023-09-28 15:19:19 +0700

    Update base.go

--- api/controllers/base.go
+++ api/controllers/base.go
@@ -55,6 +55,6 @@ func (server *Server) Initialize(Dbdriver, DbUser, DbPassword, DbPort, DbHost, D
 
 // Start the server
 func (server *Server) Run(addr string) {
-	fmt.Println("Listening to port 8080")
+	fmt.Println("Listening to port " + addr)
 	log.Fatal(http.ListenAndServe(addr, server.Router))
 }
