6a533b0b706a8d677ccbb9d78ac4b75686660443
Azriel <azrielwah@gmail.com>
2023-09-28 15:04:46 +0700

    env file optional

--- api/controllers/base.go
+++ api/controllers/base.go
@@ -45,6 +45,7 @@ func (server *Server) Initialize(Dbdriver, DbUser, DbPassword, DbPort, DbHost, D
 	}
 
 	server.DB.Debug().AutoMigrate(&models.Product{}) //database migration
+	server.DB.Debug().AutoMigrate(&models.User{})    //database migration
 
 	server.Router = mux.NewRouter()
 
--- api/server.go
+++ api/server.go
@@ -13,14 +13,18 @@ import (
 var server = controllers.Server{}
 
 func Run() {
-	var err error
-	err = godotenv.Load()
-	if err != nil {
-		log.Fatalf("Error getting env, not coming through %v", err)
+	if _, err := os.Stat(".env"); err == nil {
+		err = godotenv.Load()
+		if err != nil {
+			log.Printf("Error getting env, not coming through %v", err)
+		} else {
+			fmt.Println("env values loaded")
+		}
+	} else if os.IsNotExist(err) {
+		fmt.Println(".env file does not exist. Continuing without loading environment variables.")
 	} else {
-		fmt.Println("env values loaded")
+		log.Printf("Error checking .env file: %v", err)
 	}
-
 	server.Initialize(os.Getenv("DB_DRIVER"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_PORT"), os.Getenv("DB_HOST"), os.Getenv("DB_NAME"))
 
 	seed.Load(server.DB)
